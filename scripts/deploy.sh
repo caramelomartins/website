cb3f8df40deb00440ca432da626e162d680cac2a
Hugo Martins <caramelo.martins@gmail.com>
2017-01-06 23:30:31 +0000

    Push new content

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,13 @@
 
 echo "Building website..."
 
-cd ../public
+cd "../public"
 hugo -t cocoa
 
 echo "Copying new website into other repo..."
 
 rm -r ../../caramelomartins.github.io/*
-cp ./* ../../caramelomartins.github.io/
+cp -R ./* ../../caramelomartins.github.io/
 
 echo "Push changes to remote..."
 git add -A
