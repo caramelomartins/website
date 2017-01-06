40ee316ba41fdad4ec103e432dc6fe2bf139d391
Hugo Martins <caramelo.martins@gmail.com>
2017-01-06 23:37:49 +0000

    Push new content

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,15 +2,17 @@
 
 echo "Building website..."
 
-cd "../public"
+cd .. 
 hugo -t cocoa
+cd ../public
 
 echo "Copying new website into other repo..."
 
-rm -r ../../caramelomartins.github.io/*
 cp -R ./* ../../caramelomartins.github.io/
 
 echo "Push changes to remote..."
+
+cd "../../caramelomartins.github.io/"
 git add -A
 git commit -m "Push new content"
 git push
