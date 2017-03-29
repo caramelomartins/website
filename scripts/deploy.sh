c64ce35820ce3fc7dd46dbfa94f9c1d70ec44671
Hugo Martins <caramelo.martins@gmail.com>
2017-03-29 23:19:02 +0100

    Add new deploy script to build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Cloning caramelomartins.github.io..."
-git clone git@github.com:caramelomartins/caramelomartins.github.io.git
+git clone -q "https://caramelomartins:$GITHUB_API_KEY@github.com/caramelomartins/caramelomartins.github.io.git"
 
 echo "Removing contents..."
 rm -r caramelomartins.github.io/*
@@ -13,8 +13,8 @@ HASH=$(git rev-parse --short HEAD)
 echo "Pushing new contents..."
 cd caramelomartins.github.io || exit
 git add ./*
-git commit -m "Auto Deploy - $HASH" -m "Built from: https://github.com/caramelomartins/website/commit/$HASH." --allow-empty
-git push
+git commit -q -m "Auto Deploy - $HASH" -m "Built from: https://github.com/caramelomartins/website/commit/$HASH." --allow-empty
+git push -q "https://caramelomartins:$GITHUB_API_KEY@github.com/caramelomartins/caramelomartins.github.io.git"
 cd ..
 
 echo "Removing built assets..."
